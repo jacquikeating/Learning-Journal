1aafd372323ec748cd9a4f623b50878d9f21848d
jacquikeating <j.keating@live.ca>
2024-03-31 23:56:45 -0400

    replaced some lorem oopsum

--- scripts/index.js
+++ scripts/index.js
@@ -48,13 +48,13 @@ let post0 = new Post(0, "The Neopets to Developer Pipeline", "March 24, 2024", "
 
 let post1 = new Post(1, "Tales from Design School", "March 25, 2024", "The path to web dev is unique for everyone. Mine took a lengthy detour through graphic design.", "././images/ux.avif", "Three people writing notes at a large table covered with wireframes, sticky notes, and a laptop.")
 
-let post2 = new Post(2, "In the Beginning, There Was freeCodeCamp", "March 27, 2024", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor condimentum nunc fringilla venenatis.", "././images/fcc.png", "The freeCodeCamp logo against a plain dark background. The logo is simply the site's name in a monospace font, followed a campfire icon in parentheses.")
+let post2 = new Post(2, "In the Beginning, There Was freeCodeCamp", "March 27, 2024", "The site that launched thousands of coding careers.", "././images/fcc.png", "The freeCodeCamp logo against a plain dark background. The logo is simply the site's name in a monospace font, followed a campfire icon in parentheses.")
 
 let post3 = new Post(3, "My External Brain", "March 28, 2024", "How I built a learning system to overcome my greatest weakness.", "././images/drawers.avif", "A wall housing dozens of small wooden drawers for archiving index cards, each neatly labelled.")
 
 let post4 = new Post(4, "Jumping the JavaScript Hurdle", "March 29, 2024", "I thought I was too stupid to learn a programming language. Then I proved myself wrong.", "././images/drowning.jpg", "A stormy ocean horizon, with a single hand reaching up from the waves.")
 
-let post5 = new Post(5, "Starting with Scrimba", "March 31, 2024", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor condimentum nunc fringilla venenatis.", "././images/scrimba-logo-bg.png", "The Scrimba logo, which looks like an S made up of purple, green, and yellow rounded shapes.")
+let post5 = new Post(5, "Starting with Scrimba", "March 31, 2024", "If you're learning to code, you have got to try this.", "././images/scrimba-logo-bg.png", "The Scrimba logo, which looks like an S made up of purple, green, and yellow rounded shapes.")
 
 let post6 = new Post(6, "Teamwork Makes the Dream Work", "April 1, 2024", "My adventures in group projects and remote collaboration, plus why I think they're so important.", "././images/zoom-call-2.jpg", "A cozy study space: a table with a dotted coffee mug, a potted plant, and an open laptop showing a Zoom call with at least 14 partipants.")
 
